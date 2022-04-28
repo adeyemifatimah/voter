dac14873b54d8ac364438ae1ec4fd500204f67a1
abisoye <beesoye111@gmail.com>
2022-04-28 11:58:38 +0100

    image added to eligible page

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    const age = parseInt(prompt("How old are you???"));
+    if (age >= 21) {
+        $("#eligible").show();
+    }
+    else {
+        $("#non-eligible").show();
+    }
+    $("#vote").click(function () {
+        alert("VOTE SUCCESSFUL");
 
-    const age = parseInt(prompt("How old are you???")) ;
-
-
-
-
-
-
-
+    });
 });
+
+
+
+
+
+
+
